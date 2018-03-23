88ca34efad0638f0eeda89cd9880a372fdbd7ee5
marcbgold <marcbgold@yahoo.com>
2018-03-23 15:30:35 -0400

    added jpa test to check that tag is assigned to product

---
+++ src/test/java/org/wecancodeit/shoppingcartfullstack/catalog/ProductSearchJpaTest.java
@@ -0,0 +1,40 @@
+package org.wecancodeit.shoppingcartfullstack.catalog;
+
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class ProductSearchJpaTest {
+
+	@Resource
+	private TestEntityManager entityManager;
+
+	@Resource
+	private ProductRepository productRepo;
+
+	@Resource
+	private TagRepository tagRepo;
+
+	@Test
+	public void shouldCreateAndAssignTagToNewProduct() {
+		Tag tag = new Tag("test tag");
+		tag = tagRepo.save(tag);
+		Product product = new Product("test", tag);
+		product = productRepo.save(product);
+
+		entityManager.flush();
+		entityManager.clear();
+
+		assertThat(product.getTags(), contains(tag));
+	}
+
+}
